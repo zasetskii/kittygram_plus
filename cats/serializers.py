bcf49e1ec727705eb230e935295afec7ef70c3c7
Vlad Zasetskii <vlzaseckiy@gmail.com>
2023-01-18 01:31:40 +0300

    Кастомная сериализация поля

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-
+import datetime as dt
 from .models import Cat, Owner, Achievement, AchievementCat
+import webcolors
 
 
 class OwnerSerializer(serializers.ModelSerializer):
@@ -17,15 +18,34 @@ class AchievementSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        """Возвращаем название цвета."""
+        return value
+
+    def to_internal_value(self, data):
+        """Конвертируем hex представление в название цвета."""
+        try:
+            data = webcolors.hex_to_name(hex_value=data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = Hex2NameColor()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
-        if achievements not in self.initial_data:
+        if 'achievements' not in self.initial_data:
             cat = Cat.objects.create(**validated_data)
             return cat
         achievements = validated_data.pop('achievements')
