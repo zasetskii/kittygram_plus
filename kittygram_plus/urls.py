1b386c42253f9d8a167a98c1e81b7f2552a6d522
zasetskii <vlzaseckiy@gmail.com>
2023-01-18 23:08:14 +0300

    Добавлена аутентификация по JWT токену

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -11,4 +11,9 @@ router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
